aeb42c77a86fe3fcb87a616b885db56e3caf7eb7
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-01-30 23:06:54 -0200

    Estruturas para manipular máquinas Prisma

--- loader/include/config.h
+++ loader/include/config.h
@@ -1,21 +1,32 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#include <stdio.h>
+#include <wchar.h>
+#include <windows.h>
+#include "inireader.h"
+
+#define MAQUINANOME_TAMANHO 64
+
 typedef struct maquina_s {
-  char nome[64];
-  char nomearquivo[64];
+  wchar_t nome[MAQUINANOME_TAMANHO];
+  wchar_t nomearquivo[MAQUINANOME_TAMANHO];
 } maquina_t;
 
 typedef struct config_s {
-  FILE* file;
+  inifile_t* file;
   int count;
   maquina_t* maquinas;
 } config_t;
 
 config_t* config_criar();
 
-int config_abrir(config_t* config, char* arquivo);
+int config_abrir(config_t* config, const wchar_t* arquivo);
 
 int config_parse(config_t* config);
+
+int config_escolher(config_t* config, HINSTANCE hInstance);
+
+void config_executar(config_t* config, int id);
 
 #endif
--- loader/src/config.c
+++ loader/src/config.c
@@ -1,35 +1,129 @@
-#include <config.h>
-#include <dialogos.h>
+#include <shlwapi.h>
+#include <windows.h>
+#include <commctrl.h>
+#include "config.h"
+#include "dialogos.h"
 
-config_t* config_criar(){
-  config_t* pconfig;
-
-  pconfig = malloc(sizeof(config_t));
-  if (pconfig)
-    return pconfig
-  else
-    return NULL;
+void config_maquinascriar(config_t* config, int capacidade) {
+    config->maquinas = (maquina_t*)malloc(sizeof(maquina_t) * capacidade);
+    if (config->maquinas) {
+        config->count = capacidade;
+    }
 }
 
-int config_abrir(config_t* config, char* arquivo) {
-  FILE* file;
+void config_maquinasadicionar(config_t* config, maquina_t* maquina, int pos) {
+    wcsncpy(config->maquinas[pos].nome, maquina->nome, MAQUINANOME_TAMANHO);
+    wcsncpy(config->maquinas[pos].nomearquivo, maquina->nomearquivo, MAQUINANOME_TAMANHO);
+}
 
-  file = fopen(arquivo, "rt");
-  config->file = file;
+config_t* config_criar(){
+    config_t* config;
 
-  return (file != NULL);
+    config = (config_t*)malloc(sizeof(config_t));
+    if (config) {
+        config->file = iniparser_create();
+        if (config->file) {
+            config->count = 0;
+            config->maquinas = NULL;
+            return config;
+        }
+        else {
+            free(config);
+            return NULL;
+        }
+    } else
+        return NULL;
+}
+
+void config_destruir(config_t** pconfig) {
+    config_t* config;
+
+    config = *pconfig;
+    if (config) {
+        if (config->file)
+            iniparser_destroy(&config->file);
+        free(config);
+        config = NULL;
+    }
+}
+
+int config_abrir(config_t* config, const wchar_t* arquivo) {
+    return iniparser_open(config->file, arquivo);
 }
 
 int config_parse(config_t* config) {
-  char buffer[128];
+    int i = 0;
+    wchar_t* value;
+    maquina_t maquina;
 
-  fseek(config_t->file, sizeof(char) * 10, SEEK_SET);
-  if (fgetc(config_t->file) != '\n') {
-    msgerro("Bad file. Not newline in 10th position.");
-    return 0;
-  }
-  while (fgetc(config_t->file) != EOF) {
-    fscanf(config_t->file, "%s", buffer);
-    msgerro(buffer);
-  }
+    /* cabeçalho [INFO] */
+    if (iniparser_section(config->file, L"info")){
+        value = iniparser_readstring(config->file, L"quantidade");
+        config_maquinascriar(config, _wtoi(value));
+    } else
+        return 0;
+
+    /* cabeçalho [MAQUINA] */
+    while (i < config->count) {
+        if (iniparser_section(config->file, L"maquina")) {
+            value = iniparser_readstring(config->file, L"nome");
+            wcsncpy(maquina.nome, value, MAQUINANOME_TAMANHO);
+            value = iniparser_readstring(config->file, L"arquivo");
+            wcsncpy(maquina.nomearquivo, value, MAQUINANOME_TAMANHO);
+            config_maquinasadicionar(config, &maquina, i);
+        }
+        i++;
+    }
+
+    return (i > 0 && i == config->count);
+}
+
+TASKDIALOG_BUTTON* botoes_criar(maquina_t* maquinas, int quantidade) {
+    int i;
+    TASKDIALOG_BUTTON* botoes;
+
+    botoes = (TASKDIALOG_BUTTON*)malloc(sizeof(TASKDIALOG_BUTTON) * quantidade);
+    if (botoes) {
+        for (i = 0; i < quantidade; i++) {
+            botoes[i].nButtonID = 100 + i;
+            botoes[i].pszButtonText = maquinas[i].nome;
+        }
+    }
+
+    return botoes;
+}
+
+int config_escolher(config_t* config, HINSTANCE hInstance) {
+    int botaoPressionado = 0;
+    const TASKDIALOG_BUTTON* botoes;
+    TASKDIALOGCONFIG dialogo = {0};
+
+    botoes = botoes_criar(config->maquinas, config->count);
+    dialogo.cbSize = sizeof(dialogo);
+    dialogo.hInstance = hInstance;
+    dialogo.dwFlags = TDF_USE_COMMAND_LINKS;
+    dialogo.pszWindowTitle = L"Máquinas Prisma";
+    dialogo.pszMainIcon = TD_INFORMATION_ICON;
+    dialogo.pszMainInstruction = L"Escolha uma máquina Prisma abaixo para abrí-la.";
+    dialogo.cButtons = config->count;
+    dialogo.pButtons = botoes;
+    dialogo.pszContent = L"TEXTO.";
+    TaskDialogIndirect(&dialogo, &botaoPressionado, NULL, NULL);
+
+    return botaoPressionado;
+}
+
+void config_executar(config_t* config, int id) {
+    SHELLEXECUTEINFOW shexeInfo;
+
+    shexeInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+    shexeInfo.fMask = SEE_MASK_DEFAULT;
+    shexeInfo.hwnd = NULL;
+    shexeInfo.lpVerb = L"open";
+    shexeInfo.lpFile = config->maquinas[id-100].nomearquivo;
+    shexeInfo.lpParameters = NULL;
+    shexeInfo.lpDirectory = NULL;
+    shexeInfo.nShow = SW_SHOWNORMAL;
+    shexeInfo.hInstApp = NULL;
+    ShellExecuteExW(&shexeInfo);
 }
