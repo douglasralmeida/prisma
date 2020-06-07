187fee50ce5a8cab2bbe10597532d59f4c6d166f
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2020-06-07 20:02:36 -0300

    Atualizações do projeto

--- loader/include/dialogos.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DIALOGOS_H_
-#define _DIALOGOS_H_
-
-#include <wchar.h>
-
-void msgerro(wchar_t* msg);
-
-#endif
--- loader/src/dialogos.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-#include "dialogos.h"
-
-void msgerro(wchar_t* mensagem) {
-    MessageBoxW(NULL, mensagem, L"Carregador do Prisma", 0);
-}
